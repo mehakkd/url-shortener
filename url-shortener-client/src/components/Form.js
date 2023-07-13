32dbe53a869cde32d1fc33ce33e6a8e515632de0
Mehakkd <mehakdhaliwal5@gmail.com>
2023-07-13 10:03:26 -0400

    added css styling for index.js and form component

---
+++ url-shortener-client/src/components/Form.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { nanoid } from 'nanoid';
+import { getDatabase, child, ref, set, get } from "firebase/database";
+import { isWebUrl } from 'valid-url';
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+
+class Form extends React.Component {
+
+}
--- url-shortener-client/src/index.js
+++ url-shortener-client/src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAqQtVCHRE4DNHAEINhV4Ih2TDlXhbvdEM",
+  authDomain: "url-shortener-e670f.firebaseapp.com",
+  projectId: "url-shortener-e670f",
+  storageBucket: "url-shortener-e670f.appspot.com",
+  messagingSenderId: "1057516573680",
+  appId: "1:1057516573680:web:1afae67530e7ef766fc335",
+  measurementId: "G-1161QRWG22"
+};
+
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
